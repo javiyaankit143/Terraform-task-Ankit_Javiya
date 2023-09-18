8085c2fc11b0a2de5f643a3c12978508cd6e692c
javiyaankit143 <mr.javiya@yahoo.com>
2023-09-18 15:26:56 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "main_cidr_block" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_cidr_blocks" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+
+variable "private_cidr_blocks" {
+  type    = list(string)
+  default = ["10.0.3.0/24", "10.0.4.0/24"]
+}
